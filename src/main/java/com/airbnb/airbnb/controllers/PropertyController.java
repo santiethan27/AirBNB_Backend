18097471226906b8e507ad3d199606a24515533f
Kate_111 <katerinebuitragoyp@gmail.com>
2024-03-27 08:59:51 -0500

    FEAT: Update y Eliminar terminados}

--- src/main/java/com/airbnb/airbnb/controllers/PropertyController.java
+++ src/main/java/com/airbnb/airbnb/controllers/PropertyController.java
@@ -9,9 +9,12 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +29,6 @@ public class PropertyController {
 
     @PostMapping("/register")
     public ResponseEntity<?> registerProperty(@ModelAttribute PropertyRequest request, List<MultipartFile> images) throws Exception {
-
         try {
             if (request.getOwner() == null || request.getOwner().isEmpty()
                     || request.getDescription() == null || request.getDescription().isEmpty()
@@ -52,6 +54,24 @@ public class PropertyController {
             propertyService.createProperty(request.getOwner(), uploadedFileUrls, request.getDescription(), request.getSize(), request.getAddress(), request.getRating(), request.getPostalCode(), request.getPropertyTypes());
             return ResponseEntity.ok("Propiedad registrada exitosamente.");
         } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Collections.singletonMap("error", e.getMessage()));
+        }
+    }    
+    @PutMapping("/update-property/{id}")
+    public ResponseEntity<?> updateProperty(@PathVariable String id,@ModelAttribute PropertyRequest request)throws Exception{
+         try{
+             propertyService.updateProperty(id, request);
+             return ResponseEntity.ok("Propiedad actualizada con exito");
+         }catch (Exception e){
+             return ResponseEntity.badRequest().body(Collections.singletonMap("error", e.getMessage()));
+         }
+    }
+    @DeleteMapping("/delete-property/{id}")
+    public ResponseEntity<?> deleteProperty(@PathVariable String id) throws Exception{
+        try{
+            propertyService.deleteProperty(id);
+            return ResponseEntity.ok("Propiedad eliminada con exito");
+        }catch (Exception e){
             return ResponseEntity.badRequest().body(Collections.singletonMap("error", e.getMessage()));
         }
     }
