9f359851f29fb4785f8911b50e5d98ce0be65837
izuwashere <emanuelduque096@gmail.com>
2024-04-10 16:56:49 -0500

    creacion de nuevo crud duque

--- src/main/java/com/airbnb/airbnb/controllers/PropertyTpController.java
+++ src/main/java/com/airbnb/airbnb/controllers/PropertyTpController.java
@@ -1,14 +1,20 @@
 //Dev Duque
 package com.airbnb.airbnb.controllers;
 
+import com.airbnb.airbnb.entities.PropertyTypes;
 import com.airbnb.airbnb.requests.PropertyTpRequest;
 import com.airbnb.airbnb.servicies.PropertyTpService;
 import java.util.Collections;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,22 +29,48 @@ public class PropertyTpController {
     @Autowired
     private PropertyTpService propertyTpService;
 
-      @PostMapping("register-propertytype")
-    public ResponseEntity<?> registerBlog(@ModelAttribute PropertyTpRequest request) throws Exception {
-            try {
-            if ( request.getTitle()== null || request.getTitle().isEmpty()
+    @PostMapping("/register-propertytype")
+    public ResponseEntity<?> registerPropertyType(@ModelAttribute PropertyTpRequest request) throws Exception {
+        try {
+            if (request.getTitle() == null || request.getTitle().isEmpty()
                     || request.getImages() == null) {
-                
+
                 return ResponseEntity.badRequest().body(Collections.singletonMap("error", request));
             }
-            String uploadedUrls = cloudinaryController.upload(request.getImages());
+            String uploadedUrls = cloudinaryController.uploadSingle(request.getImages());
 
             propertyTpService.createPropertyType(request.getTitle(), uploadedUrls);
-            return ResponseEntity.ok("Blog  registrado exitosamente.");
+            return ResponseEntity.ok("Tipo de priedad registrada exitosamente.");
         } catch (Exception e) {
             System.out.println(e);
             return ResponseEntity.badRequest().body(Collections.singletonMap("error", request));
         }
+    }
+    
+      @PutMapping("/{id}")
+    public ResponseEntity<?> updatePropertyType(@PathVariable String id, @ModelAttribute PropertyTpRequest request) throws Exception {
+        try {
+            propertyTpService.updatedPropertyType(id, request);
+            return ResponseEntity.ok("Tipo de propiedad actualizada con exito");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Collections.singletonMap("error", e.getMessage()));
+        }
+    }
+    
+     @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePropertyType(@PathVariable String id) throws Exception {
+        try {
+            propertyTpService.deletePropertyType(id);
+            return ResponseEntity.ok("Tipo de propiedad eliminada con exito");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Collections.singletonMap("error", e.getMessage()));
+        }
+    }
+    
+    @GetMapping("/all-propertytype")
+      public List<PropertyTypes> getAllPropertyType(){
+        var propertyType = propertyTpService.getAllPropertyType();
+        return propertyType;
     }
 
 }
